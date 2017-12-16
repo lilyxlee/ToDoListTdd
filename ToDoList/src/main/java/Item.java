0d9590c3d7fd80042addf1f0f050992245c3739e
Sarah <lily.xia@gmail.com>
2017-12-16 17:17:20 +1100

    can add, get and compare items

--- ToDoList/src/main/java/Item.java
+++ ToDoList/src/main/java/Item.java
@@ -2,4 +2,29 @@
  * Created by Sarah on 16/12/2017.
  */
 public class Item {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    //TODO: check if the overrides are correct.
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj instanceof Item)
+           return  (((Item) obj).getDescription().equals(description)) ? true : false ;
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode() + description.hashCode();
+    }
 }
