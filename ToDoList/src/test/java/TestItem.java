cea9c768acc611bfd705294240ae76dd5fd9785c
Sarah <lily.xia@gmail.com>
2017-12-16 17:17:44 +1100

    can add, get and compare items

--- ToDoList/src/test/java/TestItem.java
+++ ToDoList/src/test/java/TestItem.java
@@ -1,5 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Sarah on 16/12/2017.
  */
 public class TestItem {
+    private String desc = "item description";
+
+    @Test
+    public void testGetItemDescription(){
+        Item item = new Item();
+        item.setDescription(desc);
+        assertEquals("Incorrect Description", desc, item.getDescription());
+    }
+
 }
